79062c00539d9469715b02e6e4c61d7fbc65dab2
JinIgarashi <igarashi.jin@water-gis.com>
2022-12-26 13:47:51 +0000

    Deploying to gh-pages from @ WASAC/watergis@c1170d64d74d0a05b265eb56746d91d238499bb0 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const m = [
-  "/_app/immutable/assets/_page-fe010636.css",
-  "/_app/immutable/chunks/singletons-d7293337.js",
-  "/_app/immutable/start-f1d4e91f.js",
-  "/_app/immutable/chunks/index-71eac763.js",
-  "/_app/immutable/components/layout.svelte-99529342.js",
+  "/_app/immutable/assets/_page-3fe5bace.css",
   "/_app/immutable/chunks/preload-helper-41c905a7.js",
-  "/_app/immutable/chunks/index-d23b2ecc.js",
-  "/_app/immutable/chunks/1-8d9436e2.js",
-  "/_app/immutable/components/error.svelte-1d9f4709.js",
-  "/_app/immutable/chunks/2-d5b480ca.js",
-  "/_app/immutable/chunks/0-016a1d45.js",
-  "/_app/immutable/chunks/_page-7e4796dc.js",
-  "/_app/immutable/chunks/index-95d721c5.js",
-  "/_app/immutable/chunks/purify.es-075a2866.js",
+  "/_app/immutable/chunks/index-71eac763.js",
   "/_app/immutable/modules/pages/_page.ts-6a8cdd0a.js",
+  "/_app/immutable/chunks/index-95d721c5.js",
+  "/_app/immutable/chunks/0-016a1d45.js",
+  "/_app/immutable/chunks/1-8d9436e2.js",
+  "/_app/immutable/chunks/2-7fa9d41d.js",
+  "/_app/immutable/components/pages/_page.svelte-e002e109.js",
+  "/_app/immutable/components/layout.svelte-99529342.js",
+  "/_app/immutable/chunks/index-d23b2ecc.js",
+  "/_app/immutable/components/error.svelte-1d9f4709.js",
+  "/_app/immutable/chunks/_page-7e4796dc.js",
+  "/_app/immutable/chunks/singletons-d7293337.js",
+  "/_app/immutable/start-07fa11ed.js",
+  "/_app/immutable/chunks/purify.es-075a2866.js",
+  "/_app/immutable/chunks/index.es-99fe28c2.js",
   "/_app/immutable/chunks/html2canvas.esm-0a33ed42.js",
-  "/_app/immutable/components/pages/_page.svelte-e0b239e5.js",
-  "/_app/immutable/chunks/index.es-685d9538.js",
-  "/_app/immutable/chunks/_page-0d4d3627.js"
+  "/_app/immutable/chunks/_page-43167218.js"
 ], h = [
   "/.nojekyll",
   "/assets/icons/icon-128x128.png",
@@ -33,7 +33,7 @@ const m = [
   "/favicon.png",
   "/manifest.webmanifest",
   "/robots.txt"
-], i = "1671979039570", a = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
+], i = "1672062419365", a = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
 a.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(o).then((e) => e.addAll(p)).then(() => {
@@ -56,17 +56,17 @@ async function r(s) {
     const t = await fetch(s);
     return e.put(s, t.clone()), t;
   } catch (t) {
-    const n = await e.match(s);
-    if (n)
-      return n;
+    const c = await e.match(s);
+    if (c)
+      return c;
     throw t;
   }
 }
 a.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, c = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !c;
-  t && !n && !l && s.respondWith(
-    (async () => c && await caches.match(s.request) || r(s.request))()
+  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), c = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !n;
+  t && !c && !l && s.respondWith(
+    (async () => n && await caches.match(s.request) || r(s.request))()
   );
 });
