cba381285fb4fc25f7788737d3e50fecba1aefe4
JinIgarashi <igarashi.jin@water-gis.com>
2023-09-30 15:58:51 +0000

    Deploying to gh-pages from @ WASAC/watergis@105cd9eafe2c86c140abc2c95756b8ad1c93073b 🚀

---
+++ _app/immutable/nodes/2.acba6656.js
@@ -0,0 +1,1 @@
+import{P as e,a as n}from"../chunks/2.d001b9a3.js";export{e as component,n as universal};
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
-const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o = [
-  s + "/_app/immutable/entry/app.519bfa51.js",
-  s + "/_app/immutable/nodes/0.3195f0d1.js",
-  s + "/_app/immutable/nodes/1.cdd23d27.js",
-  s + "/_app/immutable/nodes/2.5d3d4cda.js",
-  s + "/_app/immutable/assets/2.d75839b7.css",
-  s + "/_app/immutable/chunks/2.8c07bef3.js",
-  s + "/_app/immutable/chunks/index.63e035dc.js",
-  s + "/_app/immutable/chunks/index.a0125d55.js",
-  s + "/_app/immutable/chunks/index.b2d4e7d3.js",
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
+  s + "/_app/immutable/entry/app.19436535.js",
+  s + "/_app/immutable/nodes/0.34bf2741.js",
+  s + "/_app/immutable/nodes/1.16396af6.js",
+  s + "/_app/immutable/nodes/2.acba6656.js",
+  s + "/_app/immutable/assets/2.6f830f43.css",
+  s + "/_app/immutable/chunks/2.d001b9a3.js",
+  s + "/_app/immutable/chunks/index.40e11da7.js",
+  s + "/_app/immutable/chunks/index.a2095ca8.js",
   s + "/_app/immutable/chunks/preload-helper.a4192956.js",
-  s + "/_app/immutable/chunks/scheduler.f1554297.js",
-  s + "/_app/immutable/chunks/singletons.3d4028f6.js",
-  s + "/_app/immutable/entry/start.2604b296.js",
-  s + "/_app/immutable/chunks/index.es.99f6864c.js",
+  s + "/_app/immutable/chunks/scheduler.c37387d0.js",
+  s + "/_app/immutable/chunks/singletons.79225d70.js",
+  s + "/_app/immutable/chunks/stores.984ec711.js",
+  s + "/_app/immutable/entry/start.5eca675f.js",
+  s + "/_app/immutable/chunks/index.es.1dfbcf35.js",
   s + "/_app/immutable/chunks/purify.es.cf254a40.js",
   s + "/_app/immutable/chunks/html2canvas.esm.e0a7d97b.js"
-], u = [
+], p = [
   s + "/.nojekyll",
   s + "/assets/icons/icon-128x128.png",
   s + "/assets/icons/icon-144x144.png",
@@ -27,41 +27,51 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), o
   s + "/assets/icons/icon-72x72.png",
   s + "/assets/icons/icon-96x96.png",
   s + "/assets/preview-1200x630.png",
+  s + "/assets/tutorial/attr-table-selectbox.png",
+  s + "/assets/tutorial/eye-solid.svg",
+  s + "/assets/tutorial/isochrone-example.png",
+  s + "/assets/tutorial/isochrone-tool.png",
+  s + "/assets/tutorial/magnifying-glass-plus-solid.svg",
+  s + "/assets/tutorial/measure-tool.png",
+  s + "/assets/tutorial/palette-solid.svg",
+  s + "/assets/tutorial/routing-tool.png",
+  s + "/assets/tutorial/style-switcher.png",
+  s + "/assets/tutorial/up-down-left-right-solid.svg",
   s + "/favicon.png",
   s + "/manifest.webmanifest",
   s + "/robots.txt"
-], l = "1696022082947", i = `cache-${l}`, p = [
-  ...o,
+], u = "1696089457303", c = `cache-${u}`, o = [
+  ...l,
   // the app itself
-  ...u
+  ...p
   // everything in `static`
 ];
-self.addEventListener("install", (e) => {
-  async function n() {
-    await (await caches.open(i)).addAll(p);
+self.addEventListener("install", (t) => {
+  async function e() {
+    await (await caches.open(c)).addAll(o);
   }
-  e.waitUntil(n());
+  t.waitUntil(e());
 });
-self.addEventListener("activate", (e) => {
-  async function n() {
+self.addEventListener("activate", (t) => {
+  async function e() {
     for (const a of await caches.keys())
-      a !== i && await caches.delete(a);
+      a !== c && await caches.delete(a);
   }
-  e.waitUntil(n());
+  t.waitUntil(e());
 });
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET")
+self.addEventListener("fetch", (t) => {
+  if (t.request.method !== "GET")
     return;
-  async function n() {
-    const a = new URL(e.request.url), t = await caches.open(i);
-    if (p.includes(a.pathname))
-      return t.match(a.pathname);
+  async function e() {
+    const a = new URL(t.request.url), n = await caches.open(c);
+    if (o.includes(a.pathname))
+      return n.match(a.pathname);
     try {
-      const c = await fetch(e.request);
-      return c.status === 200 && t.put(e.request, c.clone()), c;
+      const i = await fetch(t.request);
+      return i.status === 200 && n.put(t.request, i.clone()), i;
     } catch {
-      return t.match(e.request);
+      return n.match(t.request);
     }
   }
-  e.respondWith(n());
+  t.respondWith(e());
 });
